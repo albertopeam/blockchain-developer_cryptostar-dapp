455d2fce07c83252b5cd1faa54b44cbe2aefe0b1
Alberto Penas Amor <albertopeam@gmail.com>
2021-11-13 23:32:13 +0100

    add rinkeby test network to truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,9 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraDevKey = process.env.INFURA_DEVELOPMENT_PROJECT_ID;
+const metamaskDevMnemonic = process.env.METAMASK_DEVELOPER_MNEMONIC;
 
 module.exports = {
   /**
@@ -51,7 +49,12 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-
+    rinkeby: {
+      provider: () => new HDWalletProvider(metamaskDevMnemonic, `https://rinkeby.infura.io/v3/${infuraDevKey}`),
+      network_id: 4,
+      gas: 4500000,        
+      gasPrice: 10000000000
+    },
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
